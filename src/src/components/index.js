afc33e34cc66475acdca1a681908b2140d1f10fb
emmanr <emman.info22@gmail.com>
2022-06-08 12:02:57 +0800

    removed duplicated src

--- src/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Stories } from './Stories';
-export { default as Pagination } from './Pagination/Pagination';
-export { default as Story } from './Story/Story';
-export { default as MainTitle } from './MainTitle/MainTitle';
--- src/src/helper/timestamp.js
+++
@@ -1,7 +0,0 @@
-const timestamp = (time) => {
-  const millisec = time * 1000;
-  const fullDate = new Date(millisec);
-  return fullDate.toLocaleString();
-}
-
-export default timestamp;
